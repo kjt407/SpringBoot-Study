190f20be81acb7069751b3af1018b8cc28e0e10e
kjt407 <kjt40700@gmail.com>
2021-05-03 10:43:14 +0900

    스프링부트 공부 시작

---
+++ ex1/src/test/java/com/example/ex1/SpringStudy1ApplicationTests.java
@@ -0,0 +1,14 @@
+package com.example.ex1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringStudy1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+        System.out.println("context loads");
+    }
+
+}
